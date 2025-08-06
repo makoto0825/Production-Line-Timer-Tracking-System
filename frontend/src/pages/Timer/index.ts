e0b6b37862a776ec031c03de7e51a27b692c3138
makoto082 <makototsuga@gmail.com>
2025-08-06 19:43:18 -0400

    🔥 Fix: remove Input component export from Timer index

--- frontend/src/pages/Timer/index.ts
+++ frontend/src/pages/Timer/index.ts
@@ -1,4 +1,3 @@
 export { default as Card } from './commonComponents/Card';
 export { default as Button } from './commonComponents/Button';
-export { default as Input } from './commonComponents/Input';
 export { default as InfoGrid } from './commonComponents/InfoGrid';
